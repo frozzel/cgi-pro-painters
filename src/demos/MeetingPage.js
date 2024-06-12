9a16b48c34566a3e5ab7e9da4d57e8d614702886
Dennis Hickox <frozzel@me.com>
2024-06-12 15:10:49 -0400

    testing meeting embeding

---
+++ src/demos/MeetingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import tw from "twin.macro"; //eslint-disable-line
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+
+import Hero from "components/hero/MeetingHero.js";
+
+import Footer from "components/footers/MiniCenteredFooter.js";
+
+
+export default () => (
+  <AnimationRevealPage disable>
+    <Hero />
+ 
+    <Footer />
+  </AnimationRevealPage>
+);
